f3fa03873775e859625c981137b0e78879657bac
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-09-19 11:56:51 +0100

    Added ColorBehaviour

---
+++ GD4_2022_3DGD_Introduction/Assets/GD/Scripts/ScriptableObject/ColorParameter.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "colorParameter",
+    menuName = "GD/Scriptable Objects/Parameters/Color")]
+public class ColorParameter : ScriptableObject
+{
+    public Color color;
+}
