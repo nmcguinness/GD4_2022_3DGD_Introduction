ec2918bab76a2d808af7245bd3951ad5900f9411
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-11-21 10:58:21 +0000

    :alembic: Experiment - user defined curves

--- GD4_2022_3DGD_Introduction/Assets/FadeOnEnterBehaviour.cs
+++ GD4_2022_3DGD_Introduction/Assets/FadeOnEnterBehaviour.cs
@@ -4,22 +4,22 @@ public class FadeOnEnterBehaviour : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("Select object to be faded on enter")]
-    private GameObject fadeTarget;
+    private FadeOutBehaviour fadeTarget;
 
     [SerializeField]
     [Tooltip("Specify the tag that triggers this fade")]
     private string activationTag;
 
-    private FadeOutBehaviour fadeOutBehaviour;
+    //private FadeOutBehaviour fadeOutBehaviour;
 
-    private void Start()
-    {
-        fadeOutBehaviour = fadeTarget.GetComponent<FadeOutBehaviour>();
-    }
+    //private void Start()
+    //{
+    //    fadeOutBehaviour = fadeTarget.GetComponent<FadeOutBehaviour>();
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == activationTag)
-            fadeOutBehaviour.FadeOut();
+            fadeTarget.Fade();
     }
 }
--- GD4_2022_3DGD_Introduction/Assets/FadeOutBehaviour.cs
+++ GD4_2022_3DGD_Introduction/Assets/FadeOutBehaviour.cs
@@ -2,11 +2,37 @@ using UnityEngine;
 
 public class FadeOutBehaviour : MonoBehaviour
 {
-    public float targetAlpha;
-    public float fadeTimeSecs;
+    [SerializeField]
+    [Range(0, 1)]
+    private float targetAlpha = 0;
 
-    public void FadeOut()
+    [SerializeField]
+    [Range(0, 1)]
+    private float startAlpha = 1;
+
+    [SerializeField]
+    [Range(0, 10)]
+    private float fadeTimeSecs;
+
+    [SerializeField]
+    private AnimationCurve fadeOutCurve;
+
+    [SerializeField]
+    private LeanTweenType fadeInTweenType;
+
+    private bool isFaded = false;
+
+    public void Fade()
     {
-        LeanTween.alpha(gameObject, targetAlpha, fadeTimeSecs);
+        if (!isFaded) //fade out
+        {
+            LeanTween.alpha(gameObject, targetAlpha, fadeTimeSecs).setEase(fadeOutCurve);
+            isFaded = true;
+        }
+        else         //fade in
+        {
+            LeanTween.alpha(gameObject, startAlpha, fadeTimeSecs).setEase(fadeInTweenType);
+            isFaded = false;
+        }
     }
 }
